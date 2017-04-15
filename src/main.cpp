014c5a34c1d79cd32b7ab6669e5fc3732d8f576b
qpp <q.at.nonoil@gmail.com>
2017-04-15 23:26:46 +0900

    使われていないコマンドの削除

--- src/main.cpp
+++ src/main.cpp
@@ -469,36 +469,6 @@ public:
 
 /*
 -----------------------------------------------
-@のカウントをする
------------------------------------------------
-*/
-class CommandAt
-    :public Command
-{
-public:
-    CommandAt() {}
-    virtual std::string name() override
-    {
-        return "@";
-    }
-    virtual std::string description() override
-    {
-        return "count @ num.";
-    }
-    virtual void exec(int32_t argc, char* argv[]) override
-    {
-        // todayファイルのテキストに含まれる"@"の数をカウントする
-        const std::string todayTextPath = Date(0).createFileName();
-        const std::string text = readFileAll(todayTextPath);
-        const size_t numAt = std::count_if(text.begin(), text.end(), [](char ch) { return ch == '@'; });
-        const float totalTimeInHour = float(numAt) * 0.5f;
-        printf("%2.1f hour(@ %zd)\n", totalTimeInHour, numAt);
-        getchar();
-    }
-};
-
-/*
------------------------------------------------
 ヘルプを表示する
 -----------------------------------------------
 */
@@ -622,7 +592,6 @@ void main(int32_t argc, char* argv[])
     g_commands.emplace_back(std::make_shared<CommandGoto>());
     g_commands.emplace_back(std::make_shared<CommandGrep>());
     g_commands.emplace_back(std::make_shared<CommandOpen>());
-    g_commands.emplace_back(std::make_shared<CommandAt>());
     g_commands.emplace_back(std::make_shared<CommandHelp>());
     g_commands.emplace_back(std::make_shared<CommandVersion>());
     const std::string cmd = std::string(argv[1]);
